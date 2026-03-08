ebe82f106b0112af8a8fb2c374675c0b2c03762c
Owen Kitchener <okitchener@my.wctc.edu>
2026-03-07 22:18:31 -0600

    Assignment 6 Done

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Northwind.Controllers
 {
@@ -7,7 +8,28 @@ namespace Northwind.Controllers
         private readonly DataContext _dataContext = db;
 
         public IActionResult Category(){
-            return View(_dataContext.Categories);
+            List<Category> categories = _dataContext.Categories
+                .OrderBy(c => c.CategoryName)
+                .ToList();
+
+            return View(categories);
+        }
+
+        public IActionResult Index(int id)
+        {
+            Category? category = _dataContext.Categories.FirstOrDefault(c => c.CategoryId == id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.CategoryName = category.CategoryName;
+            List<Product> products = _dataContext.Products
+                .Where(p => p.CategoryId == id && !p.Discontinued)
+                .OrderBy(p => p.ProductName)
+                .ToList();
+
+            return View(products);
         }
     }
 
